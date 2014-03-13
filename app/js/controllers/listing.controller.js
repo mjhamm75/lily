eab75ac0671eafe40c0968b66ebfc51efb0c0bc8
Jason Hamm <jhamm.business@gmail.com>
2014-03-13 05:56:24 -0400

    Add mock data to scope for angular

--- app/js/controllers/listing.controller.js
+++ app/js/controllers/listing.controller.js
@@ -1,3 +1,16 @@
-app.controller('ListingCtrl', function() {
-
+app.controller('ListingCtrl', function($scope) {
+  $scope.scouts = [
+    {
+      "name": "Jason Hamm",
+      "rank": "Tenderfoot"
+    },
+    {
+      "name": "Whitney Palmer",
+      "rank": "Star"
+    },
+    {
+      "name": "Tristan McGraw",
+      "rank": "Life"
+    }
+  ]
 });
