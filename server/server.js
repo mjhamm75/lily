9eb7d45e56a3647746e912305278fba3ea4332bb
Jason Hamm <jhamm.business@gmail.com>
2014-03-13 06:00:18 -0400

    Add port variable

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var port = process.env.PORT || 9000;
 
 var app = express();
 
@@ -6,4 +7,7 @@ app.get('*', function(request, response) {
     response.send("This would be some more HTML");
 });
 
-app.listen(9000);
+app.listen(port, function() {
+  'use strict';
+  console.log('Listening on:', port);
+});
