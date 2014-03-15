08fdc1d5bd0e16d38697eee863ddb82c485c1559
Jason Hamm <jhamm.business@gmail.com>
2014-03-15 11:37:01 -0400

    Add ngResource as dependency to be able to create REST services

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('LilyApp', ['ngRoute']);
+var app = angular.module('LilyApp', ['ngRoute', 'ngResource']);
 
 app.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/', {
