c6773f2d996fa3e6e08661d7b6fb30b5a7a246d9
Jason Hamm <jhamm.business@gmail.com>
2014-03-15 22:53:59 -0400

    Show all of scouts in table

--- app/js/services/scouts.service.js
+++ app/js/services/scouts.service.js
@@ -1,5 +1,4 @@
 app.factory('scoutService', function($resource) {
-  return $resource('http://99.44.242.76:3000/scouts/:scoutId', {scoutId: '@scoutId'}, {
-
+  return $resource('../scouts/:scoutId', {scoutId: '@scoutId'}, {
   });
 });
