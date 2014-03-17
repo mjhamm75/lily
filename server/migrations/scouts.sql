c5a3c78dd11cec5e61e9a9b619c7e7aaf7f28a33
Jason Hamm <jhamm.business@gmail.com>
2014-03-17 05:58:45 -0400

    Create sql for needed tables

--- server/migrations/scouts.sql
+++ server/migrations/scouts.sql
@@ -2,9 +2,9 @@ CREATE TYPE rank AS ENUM('Boy Scout', 'Tenderfoot', 'Second Class', 'First Class
 
 CREATE TABLE scouts
 (
-  user_id          serial primary key,
-  firstName      varchar(75) NOT NULL,
-  lastName       varchar(75) NOT NULL,
+  scout_id          serial primary key,
+  first_name      varchar(75) NOT NULL,
+  last_name       varchar(75) NOT NULL,
   rank              rank,
   email             varchar(75),
   password       varchar(75),
@@ -12,4 +12,37 @@ CREATE TABLE scouts
   city                varchar(50),
   state              char(2),
   zip                 varchar(10)
+);
+
+CEATE TYPE advancement_type AS ENUM('Rank', 'Merit Badge');
+
+CREATE TABLE advancements
+(
+  advancement_id  serial primary key,
+  name                  varchar(12),
+  eagle_required     boolean,
+  type                    advancement_type
+);
+
+CREATE TABLE scout_advancements
+(
+  scout_id    integer references scouts (scout_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  advancement_id    integer references advancements (advancement_id) ON UPDATE CASCADE ON DELETE CASCADE,
+);
+
+CREATE TABLE requirements
+(
+  requirement_id  serial primary key
+);
+
+CREATE TABLE advancement_requirements
+(
+  advancement_id    integer references advancements (advancement_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  requirement_id    integer references requirements (requirement_id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE scout_requirements
+(
+  scout_id    integer references scouts (scout_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  requirement_id    integer references requirements (requirement_id) ON UPDATE CASCADE ON DELETE CASCADE
 );
