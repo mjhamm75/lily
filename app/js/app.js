11904d07f167f3454fbce21b8b48c6ab5a86abd6
Jason Hamm <jhamm.business@gmail.com>
2014-03-19 06:01:59 -0400

    Add routing and page for single scout

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,14 @@
 var app = angular.module('LilyApp', ['ngRoute', 'ngResource']);
 
 app.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/', {
-    controller: 'ListingCtrl',
-    templateUrl: 'templates/listing.html'
+  $routeProvider
+  .when('/', {
+    controller: 'ScoutsCtrl',
+    templateUrl: 'templates/scouts.html'
+  })
+  .when('/:id', {
+    controller: 'ScoutsCtrl',
+    templateUrl: 'templates/scout.html'
   })
   .otherwise({redirectTo: '/'});
 }]);
