fbfdccfe9c4a0dcebf3dcae8c4e61c400a34cc9a
Jason Hamm <jhamm.business@gmail.com>
2014-03-19 06:03:53 -0400

    Add comment on ugly hack

--- app/js/controllers/scouts.controller.js
+++ app/js/controllers/scouts.controller.js
@@ -4,6 +4,7 @@ app.controller('ScoutsCtrl', function($scope, $location, scoutService) {
   });
 
   $scope.showScout = function(scout) {
+    // I dont understand why I need this ugly hack with the empty ''
     $location.path('' + scout.id);
   };
 });
