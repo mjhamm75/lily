bd7abd332491b74b8f13a3d5057294624773a000
Jason Hamm <jason.hamm@ldschurch.org>
2014-04-08 06:20:10 -0600

    Add new route to get advancement detail

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
+/* global require, process*/
+
 var express = require('express');
 require('./config/bookshelf.js');
 var port = process.env.PORT || 9000;
 var scouts = require('./routes/scouts.js');
+var advancements = require('./routes/advancements.js');
 
 var app = express();
 app.use(express.bodyParser());
@@ -10,8 +13,10 @@ app.get('/scouts/:id', scouts.getScout);
 app.get('/scouts', scouts.getScouts);
 app.post('/scouts', scouts.createScout);
 
+app.get('/advancements/:id', advancements.getAdvancement);
+
 app.get('*', function(req, res) {
-    res.send("This would be some more HTML");
+  res.send("This would be some more HTML");
 });
 
 app.listen(port, function() {
