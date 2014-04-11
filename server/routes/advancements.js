9e0534c921b90df801da9004637ca2b5a71311a3
Jason Hamm <jason.hamm@ldschurch.org>
2014-04-10 20:26:15 -0600

    Add id to json object

--- server/routes/advancements.js
+++ server/routes/advancements.js
@@ -1,14 +1,26 @@
-/* global exports */
+/* global exports, require */
 
 var Bookshelf = require('bookshelf').PG;
-
-var Advancement = Bookshelf.Model.extend({
-  tableName: 'advancements'
-});
+var _ = require('underscore');
 
 exports.getAdvancement = function(req, res) {
-  var advancement = new Advancement({id: req.params.id});
-  advancement.fetch().then(function(data) {
-    res.json(data);
-  });
+  Bookshelf.knex('requirements')
+    .join('advancement_requirements', 'requirements.internal_id', '=', 'advancement_requirements.requirement_id')
+    .where({
+      'advancement_requirements.advancement_id': req.params.id
+    })
+    .then(function(data) {
+      var requirements = _.each(data, function(d) {
+        var r = {
+          id: d.id,
+          description: d.description
+        };
+        return r;
+      });
+      var result = {
+        meta: {},
+        requirements: requirements
+      };
+      res.json(result);
+    });
 };
