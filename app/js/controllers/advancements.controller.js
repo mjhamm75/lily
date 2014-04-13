3e8570ca226585ea56a9f2f2d758410e76bc418b
Jason Hamm <jason.hamm@ldschurch.org>
2014-04-12 23:10:21 -0600

    Add logic when completing a requirement

--- app/js/controllers/advancements.controller.js
+++ app/js/controllers/advancements.controller.js
@@ -1,10 +1,20 @@
 /* global app */
 
-app.controller('AdvancementsCtrl', function($scope, $location, $routeParams, advancementService) {
+app.controller('AdvancementsCtrl', function($scope, $location, $routeParams, advancementService, requirementsService, scoutService) {
 
   if($routeParams.id && advancementService.advancement === undefined) {
     advancementService.getAdvancement($routeParams.id).then(function(data) {
       $scope.advancement = data;
     });
   }
+
+  $scope.completeRequirement = function(req) {
+    requirementsService.updateRequirement(req.id, scoutService.scout.id, function(data) {
+      if(data.completed_date) {
+        req.completed_date = data.completed_date;
+      } else {
+        req.completed_date = null;
+      }
+    });
+  };
 });
