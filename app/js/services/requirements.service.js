30edd51d5b521c54d073456cb46801bd7660700e
Jason Hamm <jason.hamm@ldschurch.org>
2014-04-14 05:38:19 -0600

    Add callback to toggle req

--- app/js/services/requirements.service.js
+++ app/js/services/requirements.service.js
@@ -4,10 +4,12 @@ app.factory('requirementsService', function($resource, $q) {
   return {
     _getResource: $resource('../requirements/:requirementId', {requirementId: '@requirementId'}, {
     }),
-    updateRequirement: function(requirementId, scoutId) {
+    updateRequirement: function(requirementId, scoutId, callback) {
       this._getResource.save({
         requirementId: requirementId,
         scoutId: scoutId
+      }, function(data) {
+        callback(data);
       });
     }
   };
