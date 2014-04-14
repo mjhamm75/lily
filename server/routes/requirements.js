4fcda4013ccfeefae136b8945137ee2f6cab68f9
Jason Hamm <jason.hamm@ldschurch.org>
2014-04-14 05:40:19 -0600

    Add ability to toggle if a req is finished

--- server/routes/requirements.js
+++ server/routes/requirements.js
@@ -1,28 +1,34 @@
 /* global exports, require */
 
 var Bookshelf = require('bookshelf').PG;
-var _ = require('underscore');
 
-exports.getRequirements = function(req, res) {
-  Bookshelf.knex('requirements')
-    .join('advancement_requirements', 'requirements.internal_id', '=', 'advancement_requirements.requirement_id')
-    .where({
-      'advancement_requirements.advancement_id': req.params.id
-    })
-    .then(function(data) {
-      var requirements = _.each(data, function(d) {
-        var r = {
-          id: d.id,
-          description: d.description
-        };
-        return r;
+var ScoutRequirements = Bookshelf.Model.extend({
+  tableName: 'scout_requirements',
+  idAttribute: null
+});
+
+exports.toggleRequirement = function(req, res) {
+  var scoutRequirements = new ScoutRequirements({
+    scout_id: req.body.scoutId,
+    requirement_id: req.body.requirementId,
+  });
+  scoutRequirements.fetch().then(function(data) {
+    if(data === null) {
+      scoutRequirements.set('initials', 'mjh');
+      scoutRequirements.set('completed_date', '2014-04-09');
+      scoutRequirements.save().then(function(data) {
+        res.json(data);
       });
-      var result = {
-        meta: {
-          count: requirements.length
-        },
-        requirements: requirements
-      };
-      res.json(result);
-    });
+    } else {
+      console.log('KNEX');
+      Bookshelf.knex('scout_requirements')
+        .where('scout_id', '=', req.body.scoutId)
+        .where('requirement_id', '=', req.body.requirementId)
+        .del().then(function(data) {
+          res.json({
+            deleted: true
+          });
+        });
+    }
+  });
 };
