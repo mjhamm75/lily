dc8f3bd0373276f5a814b2c3e184e594231532d1
Jason Hamm <jason.hamm@ldschurch.org>
2014-04-15 17:50:14 -0600

    Reimplement toggleRequirement to make less hits on db

--- server/routes/requirements.js
+++ server/routes/requirements.js
@@ -2,32 +2,43 @@
 
 var Bookshelf = require('bookshelf').PG;
 
-var ScoutRequirements = Bookshelf.Model.extend({
+var ScoutRequirement = Bookshelf.Model.extend({
   tableName: 'scout_requirements',
   idAttribute: null
 });
 
+var ScoutRequirements = Bookshelf.Collection.extend({
+  model: ScoutRequirement
+});
+
 exports.toggleRequirement = function(req, res) {
-  var scoutRequirement = new ScoutRequirements({
-    scout_id: req.body.scoutId,
-    requirement_id: req.body.requirementId,
-  });
-  scoutRequirement.fetch().then(function(data) {
-    if(data === null) {
+  var scoutRequirements = new ScoutRequirements();
+  scoutRequirements.query({
+    where: {
+      scout_id: req.body.scoutId
+    }
+  }).fetch().then(function(data) {
+    var scoutRequirement = data.find(function(model) {
+      return model.get('requirement_id') === req.body.requirementId;
+    });
+    if(scoutRequirement === undefined) {
+
+      var scoutRequirement = new ScoutRequirement();
+      scoutRequirement.set('requirement_id', req.body.requirementId);
+      scoutRequirement.set('scout_id', req.body.scoutId);
       scoutRequirement.set('initials', 'mjh');
       scoutRequirement.set('completed_date', '2014-04-09');
       scoutRequirement.save().then(function(data) {
         res.json(data);
       });
     } else {
-      Bookshelf.knex('scout_requirements')
-        .where('scout_id', '=', req.body.scoutId)
-        .where('requirement_id', '=', req.body.requirementId)
-        .del().then(function(data) {
-          res.json({
-            deleted: true
-          });
-        });
+      scoutRequirement.where({
+        requirement_id: red.body.requirementId
+      }).destroy().then(function(data) {
+        res.json({
+          deleted: true
+        })
+      })
     }
   });
-};
+}
