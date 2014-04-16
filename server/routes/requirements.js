06cdef34322e50c5bc034bcb51af9ce9ad580f5e
Jason Hamm <jason.hamm@ldschurch.org>
2014-04-16 06:38:20 -0600

    Remove commented out code

--- server/routes/requirements.js
+++ server/routes/requirements.js
@@ -21,40 +21,6 @@ var AdvancementRequirements = Bookshelf.Collection.extend({
   model: AdvancementRequirement
 });
 
-// exports.toggleRequirement = function(req, res) {
-//   var scoutRequirements = new ScoutRequirements();
-//   scoutRequirements.query({
-//     where: {
-//       scout_id: req.body.scoutId
-//     }
-//   }).fetch().then(function(data) {
-//     var scoutRequirement = data.find(function(model) {
-//       return model.get('requirement_id') === req.body.requirementId;
-//     });
-//     if(scoutRequirement === undefined) {
-
-//       var scoutRequirement = new ScoutRequirement({
-//         requirement_id: req.body.requirementId,
-//         scout_id: req.body.scoutId,
-//         initials: 'mjh',
-//         completed_date: '2014-04-10'
-//       });
-
-//       scoutRequirement.save().then(function(data) {
-//         res.json(data);
-//       });
-//     } else {
-//       Bookshelf.knex('scout_requirements')
-//         .where('scout_id', req.body.scoutId)
-//         .where('requirement_id', req.body.requirementId)
-//         .del().then(function(data) {
-//           res.json({
-//             deleted: true
-//           })
-//         });
-//     }
-//   });
-
 var toggleScoutRequirement = function(requirementId, scoutId, scoutRequirement, callback) {
   if(scoutRequirement === undefined) {
     var scoutRequirement = new ScoutRequirement({
