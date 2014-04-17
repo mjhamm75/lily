c12f0360635bd9cffda9df73bc54adad8d60ead1
Jason Hamm <jason.hamm@ldschurch.org>
2014-04-17 07:41:54 -0600

    Update requirement in service

--- app/js/services/requirements.service.js
+++ app/js/services/requirements.service.js
@@ -3,9 +3,14 @@
 app.factory('requirementsService', function($resource, $q) {
   return {
     _getResource: $resource('../requirements/:requirementId', {requirementId: '@requirementId'}, {
+      save: {
+        method: 'POST',
+        isArray: true
+      }
     }),
-    updateRequirement: function(requirementId, scoutId, callback) {
+    updateRequirements: function(requirementId, scoutId, advancementId, callback) {
       this._getResource.save({
+        advancementId: advancementId,
         requirementId: requirementId,
         scoutId: scoutId
       }, function(data) {
