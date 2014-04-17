c38d6cdcf4daad2c0810f6898cf1835a3309032b
Jason Hamm <jason.hamm@ldschurch.org>
2014-04-17 07:49:56 -0600

    Remove bootstrap.ui

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('LilyApp', ['ngRoute', 'ngResource', 'ui.bootstrap']);
+var app = angular.module('LilyApp', ['ngRoute', 'ngResource']);
 
 app.config(['$routeProvider', function($routeProvider) {
   $routeProvider
