bdcf5f5e6c7699eb3da1000e0639a0f1b76437d8
Jason Hamm <jason.hamm@ldschurch.org>
2014-05-30 07:44:51 -0400

    Extract methods into common file

--- server/common/common.js
+++ server/common/common.js
@@ -1,5 +1,75 @@
 /* global exports */
 var _ = require('underscore');
+var Bookshelf = require('bookshelf').PG;
+
+var AdvancementRequirement = Bookshelf.Model.extend({
+  tableName: 'advancement_requirements',
+  idAttribute: null
+});
+
+var AdvancementRequirements = Bookshelf.Collection.extend({
+  model: AdvancementRequirement
+});
+
+var ScoutRequirement = Bookshelf.Model.extend({
+  tableName: 'scout_requirements',
+  idAttribute: null
+});
+
+var ScoutRequirements = Bookshelf.Collection.extend({
+  model: ScoutRequirement
+});
+
+exports.getScoutRequirements = function(scoutId, callback) {
+  'use strict';
+  var scoutRequirements = new ScoutRequirements();
+  scoutRequirements.query({
+    where: {
+      scout_id: scoutId
+    }
+  }).fetch().then(function(data) {
+    callback(data);
+  });
+};
+
+exports.updateScoutRequirement = function(reqId, scoutId, callback) {
+  'use strict';
+  var scoutReq = new ScoutRequirement({
+      requirement_id: reqId,
+      scout_id: scoutId,
+      initials: 'mjh',
+      completed_date: '2014-04-10'
+    });
+    scoutReq.save().then(function(data) {
+      callback(data.toJSON());
+    });
+};
+
+exports.deleteScoutRequirement = function(reqId, scoutId, callback) {
+  'use strict';
+  Bookshelf.knex('scout_requirements')
+      .where('scout_id', scoutId)
+      .where('requirement_id', reqId)
+      .del().then(function() {
+        callback({
+          requirement_id: reqId,
+          completed_date: null,
+          initials: null
+        });
+      });
+};
+
+exports.getAdvancementRequirements = function(advancementId, callback) {
+  'use strict';
+  var advancementRequirements = new AdvancementRequirements();
+  advancementRequirements.query({
+    where: {
+      advancement_id: advancementId
+    }
+  }).fetch().then(function(data) {
+    callback(data);
+  });
+};
 
 exports.combineRequirementsWithScoutRequirements = function(advancementRequirements, scoutRequirementsComplete) {
   'use strict';
--- server/routes/requirements.js
+++ server/routes/requirements.js
@@ -1,95 +1,26 @@
 /* global exports, require */
 
-var Bookshelf = require('bookshelf').PG;
 var async = require('async');
 var _ = require('underscore');
-
-var ScoutRequirement = Bookshelf.Model.extend({
-  tableName: 'scout_requirements',
-  idAttribute: null
-});
-
-var ScoutRequirements = Bookshelf.Collection.extend({
-  model: ScoutRequirement
-});
-
-var AdvancementRequirement = Bookshelf.Model.extend({
-  tableName: 'advancement_requirements',
-  idAttribute: null
-});
-
-var AdvancementRequirements = Bookshelf.Collection.extend({
-  model: AdvancementRequirement
-});
-
-var updateScoutRequirement = function(reqId, scoutId, callback) {
-  'use strict';
-  var scoutReq = new ScoutRequirement({
-      requirement_id: reqId,
-      scout_id: scoutId,
-      initials: 'mjh',
-      completed_date: '2014-04-10'
-    });
-    scoutReq.save().then(function(data) {
-      callback(data.toJSON());
-    });
-};
-
-var deleteScoutRequirement = function(reqId, scoutId, callback) {
-  'use strict';
-  Bookshelf.knex('scout_requirements')
-      .where('scout_id', scoutId)
-      .where('requirement_id', reqId)
-      .del().then(function() {
-        callback({
-          requirement_id: reqId,
-          completed_date: null,
-          initials: null
-        });
-      });
-};
+var common = require('./../common/common.js');
 
 var toggleScoutRequirement = function(scoutId, scoutRequirement, advancementRequirement, callback) {
   'use strict';
   if(scoutRequirement === undefined) {
-    updateScoutRequirement(advancementRequirement.requirement_id, scoutId, function(data) {
+    common.updateScoutRequirement(advancementRequirement.requirement_id, scoutId, function(data) {
       callback(data);
     });
   } else {
     var id = advancementRequirement.requirement_id;
-    deleteScoutRequirement(id, scoutId, function(data) {
+    common.deleteScoutRequirement(id, scoutId, function(data) {
       callback(data);
     });
   }
 };
 
-var getScoutRequirements = function(scoutId, callback) {
-  'use strict';
-  var scoutRequirements = new ScoutRequirements();
-  scoutRequirements.query({
-    where: {
-      scout_id: scoutId
-    }
-  }).fetch().then(function(data) {
-    callback(data);
-  });
-};
-
-var getAdvancementRequirements = function(advancementId, callback) {
-  'use strict';
-  var advancementRequirements = new AdvancementRequirements();
-  advancementRequirements.query({
-    where: {
-      advancement_id: advancementId
-    }
-  }).fetch().then(function(data) {
-    callback(data);
-  });
-};
-
 var getChildrenComplete = function(scoutId, children, callback) {
   'use strict';
-  getScoutRequirements(scoutId, function(scoutReqs) {
+  common.getScoutRequirements(scoutId, function(scoutReqs) {
     var count = 0;
     async.each(children, function(child, callback) {
       var req = _.findWhere(scoutReqs.toJSON(), { requirement_id: child});
@@ -142,12 +73,12 @@ var toggleCurrentParent = function(scoutId, reqId, scoutReqs, advancementReqs, c
     var children_needed = parentReq.children_needed;
     getChildrenComplete(scoutId, parentReq.children, function(children_complete) {
       if(children_needed === children_complete) {
-        updateScoutRequirement(parentReq.requirement_id, scoutId, function(data) {
+        common.updateScoutRequirement(parentReq.requirement_id, scoutId, function(data) {
           callback(data);
         });
       } else {
         var id = parentReq.requirement_id;
-        deleteScoutRequirement(id, scoutId, function(data) {
+        common.deleteScoutRequirement(id, scoutId, function(data) {
           callback(data);
         });
       }
@@ -161,12 +92,12 @@ exports.toggleRequirement = function(req, res) {
   'use strict';
   async.parallel({
     scoutRequirements: function(callback) {
-      getScoutRequirements(req.body.scoutId, function(data) {
+      common.getScoutRequirements(req.body.scoutId, function(data) {
         callback(null, data);
       });
     },
     advancementRequirements: function(callback){
-      getAdvancementRequirements(req.body.advancementId, function(data) {
+      common.getAdvancementRequirements(req.body.advancementId, function(data) {
         callback(null, data);
       });
     }
